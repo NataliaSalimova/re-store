7a0d338ad919d602b4fcc6e3b3f58cf0caa36ee3
nsalimova <nsalimova@oggettoweb.com>
2023-11-12 00:08:19 +0300

    Reading data from Redux Store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 const initialState = {
-    books: []
+    books: [
+        {
+            id: 1,
+            title: 'Book 1',
+            author: 'Author 1'
+        },
+        {
+            id: 2,
+            title: 'Book 2',
+            author: 'Author 2'
+        }
+    ]
 };
 
 const reducer = (state = initialState, action)=> {
