3cbed5c9f7f95cab38e429d9459566b342e386bb
nsalimova <nsalimova@oggettoweb.com>
2023-11-12 15:11:13 +0300

    Difference setState and reducer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,18 +4,17 @@ import Spinner from '../spinner';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 import { compose } from '../../utils';
 import './book-list.css';
 
 class BookList extends Component {
 
     componentDidMount() {
-        const { bookstoreService, booksLoaded } = this.props;
+        const { bookstoreService, booksLoaded, booksRequested } = this.props;
+        booksRequested();
         bookstoreService.getBooks()
-            .then((data)=> {
-                booksLoaded(data);
-            });
+            .then((data)=> booksLoaded(data));
     }
 
     render() {
@@ -44,7 +43,8 @@ const mapStateToProps = ({ books, loading })=> {
 };
 
 const mapDispatchToProps = {
-    booksLoaded
+    booksLoaded,
+    booksRequested
 };
 
 export default compose(
