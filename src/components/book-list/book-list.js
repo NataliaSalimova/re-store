8935309d9ddb577e5f42d068594becb211f16e64
nsalimova <nsalimova@oggettoweb.com>
2023-11-13 22:27:10 +0300

    Naming Convention for actions

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,19 +5,17 @@ import ErrorIndicator from '../error-indicator';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { compose } from '../../utils';
 import './book-list.css';
 
-
 class BookList extends Component {
-
     componentDidMount() {
         this.props.fetchBooks();
     }
 
     render() {
-        const { books, loading, error} = this.props;
+        const { books, loading, error } = this.props;
 
         if (loading) {
             return <Spinner/>
@@ -38,22 +36,16 @@ class BookList extends Component {
                 }
             </ul>
         )
-    }
+    };
 }
 
 const mapStateToProps = ({ books, loading, error })=> {
     return { books, loading, error };
 };
 
-const mapDispatchToProps = (dispatch, ownProps)=> {
-    const { bookstoreService } = ownProps;
+const mapDispatchToProps = (dispatch, { bookstoreService })=> {
     return {
-        fetchBooks: ()=> {
-            dispatch(booksRequested());
-            bookstoreService.getBooks()
-                .then((data)=> dispatch(booksLoaded(data)))
-                .catch((err)=> dispatch(booksError(err)));
-        }
+        fetchBooks: fetchBooks(bookstoreService, dispatch)
     }
 };
 
